36524db42733ed3138830a435d18c41c04dc0b75
RJ GAMING 20 <120710807+RjGamer20@users.noreply.github.com>
2022-12-16 13:46:30 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const BOT = require("./handlers/Client");
+const { token } = require("./settings/config");
+
+const client = new BOT();
+
+module.exports = client;
+
+client.build(token);
+
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+  console.log(" [Error_Handling] :: Multiple Resolves");
+  console.log(reason);
+});
